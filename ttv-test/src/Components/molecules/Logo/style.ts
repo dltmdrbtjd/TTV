85f5eed854471286d7867b065ea7150bd4a4036c
dltmdrbtjd <dltmdrbtjd@gmail.com>
2021-09-16 01:04:14 +0900

    상단 header logo추가

---
+++ ttv-test/src/Components/molecules/Logo/style.ts
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+import Grid from '../../atom/Grid';
+
+export const Logo = styled(Grid)`
+  background-image: url('http://ttv.waynehills.co/assets/logo.png');
+  background-position: center;
+  background-size: cover;
+  background-color: black;
+  width: 150px;
+  height: 42px;
+  filter: invert(1);
+`;

b96cbd323bedcb3e48d2c7fb9818bedc0c3d9864
dltmdrbtjd <dltmdrbtjd@gmail.com>
2021-09-16 01:04:01 +0900

    atom/Text P tag 추가

---
+++ ttv-test/src/Components/atom/Text/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import * as S from './style';
+
+export type Props = {
+  children: any;
+  className?: string;
+};
+
+function Text({ children, className }: Props) {
+  return <S.Text className={className}>{children}</S.Text>;
+}
+
+export default Text;

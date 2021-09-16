4a4fa6da7b6044e7b99586f6e77c6b272d54419c
dltmdrbtjd <dltmdrbtjd@gmail.com>
2021-09-17 02:53:58 +0900

    메인페이지 컴포넌트 생성

--- ttv-test/src/pages/Main/index.tsx
+++ ttv-test/src/pages/Main/index.tsx
@@ -1,26 +1,24 @@
 import React from 'react';
 import MainTitle from '../../Components/molecules/MainTitle';
-import Scripting from '../../Components/molecules/Scripting';
 import Header from '../../Components/orgarnisms/Header';
 import SwiperBox from '../../Components/orgarnisms/SwiperBox';
+import EncodingWrap from '../../Components/orgarnisms/EncodingWrap';
+import * as S from './style';
+import Footer from '../../Components/orgarnisms/Footer';
+import Modal from '../../Components/orgarnisms/Modal';
 
 function Main() {
   return (
     <>
-      <div style={{ maxWidth: '1500px', margin: '0 auto' }}>
-        <Header />
-        <div
-          style={{
-            display: 'flex',
-            justifyContent: 'space-between',
-            alignItems: 'center',
-            width: '1300px',
-          }}
-        >
-          <SwiperBox />
-          <MainTitle />
+      <S.MainWrap>
+        <div>
+          <Header />
+          <S.SwiperSection>
+            <SwiperBox />
+            <MainTitle />
+          </S.SwiperSection>
         </div>
-      </div>
+      </S.MainWrap>
       <svg
         _ngcontent-lur-c93=""
         xmlns="http://www.w3.org/2000/svg"
@@ -28,7 +26,6 @@ function Main() {
         preserveAspectRatio="none"
         viewBox="0 0 1680 40"
         id="svg-top"
-        className="position-absolute width-full z-1"
       >
         <path
           _ngcontent-lur-c93=""
@@ -36,7 +33,28 @@ function Main() {
           fill="#000"
         />
       </svg>
-      <Scripting />
+      <EncodingWrap />
+      <svg
+        _ngcontent-qpv-c93=""
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        preserveAspectRatio="none"
+        viewBox="0 0 1680 40"
+        id="svg-bottom"
+        style={{ transform: 'scaleY(-1) translateY(1px)' }}
+      >
+        <path
+          _ngcontent-qpv-c93=""
+          d="M0 40h1680V30S1340 0 840 0 0 30 0 30z"
+          fill="#000"
+        />
+      </svg>
+      <S.Powered>powered by</S.Powered>
+      <S.PoweredBox>
+        <S.PoweredImg src="https://upload.wikimedia.org/wikipedia/commons/c/cd/Shutterstock_logo.svg" />
+      </S.PoweredBox>
+      <Footer />
+      <Modal />
     </>
   );
 }
